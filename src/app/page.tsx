b30cbeb703873d3599890c70cf2cdb5908e49ba4
Joy Das <joy600508@gmail.com>
2025-01-03 19:33:35 +0600

    code was updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import MessageBox from "@/components/chatboard/messagebox/MessageBox";
 import ChartSidebar from "@/components/chatboard/shared/ChartSidebar";
 const WellCome = () => {
   return (
-    <div className="p-14">
+    <div className="px-20 py-8">
       <div className="flex border border-gray-700 rounded-lg ">
         <div className="">
           <ChartSidebar />
